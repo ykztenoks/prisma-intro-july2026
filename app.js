266c4d98f5d6aaa1d8e8879c4a3a7d6b87ab92b7
ykztenoks <danielskonetzky7@gmail.com>
2026-07-07 07:10:29 -0300

    done :thumbs_up:

---
+++ app.js
@@ -0,0 +1,57 @@
+// const express = require("express")
+
+import express from "express"
+import morgan from "morgan"
+import { prisma } from "./lib/prisma.js"
+const app = express()
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ msg: "Server is running" })
+})
+
+app.get("/users", async (req, res) => {
+  try {
+    const response = await prisma.user.findMany()
+
+    console.log(response)
+
+    return res.status(200).json(response)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+})
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const foundUser = await prisma.user.findUnique({ where: { id: id } })
+
+    return res.status(200).json(foundUser)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+})
+
+app.post("/users", async (req, res) => {
+  try {
+    const { username, email } = req.body
+
+    const response = await prisma.user.create({ data: { username, email } })
+
+    console.log(response)
+    return res.status(201).json({ msg: "user created", user: response })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+})
+
+app.listen(8080, () => {
+  console.clear()
+  console.log("Server running on port 8080")
+})
---
+++ prisma/migrations/20260707092100_init_dif_db_url/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" VARCHAR(24) NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "post" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
